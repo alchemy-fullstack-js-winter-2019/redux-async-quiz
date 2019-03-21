4d84e1d492d8d61a0fc724bdecdb6113125178ff
Kristin Hortsch <kristinhortsch@gmail.com>
2019-03-21 09:14:29 -0700

    passing selectors test

--- src/reducers/dogs.js
+++ src/reducers/dogs.js
@@ -4,6 +4,8 @@ const initialState = {
 
 export default function reducer(state = initialState, action) {
   switch(action.type) {
+    case 'FETCH_DOG_IMAGE':
+      return action.payload;
     default: 
       return state;
   }
