e05313a4806c194c54ccdc4de960bfe07843e106
Carmen V Ramos <carmenvramos@yahoo.com>
2019-03-21 12:12:43 -0700

    Fixed failing Image test

--- src/components/Image.test.js
+++ src/components/Image.test.js
@@ -4,10 +4,10 @@ import Image from './Image';
 
 describe('Image test', () => {
   it('matches a snapshot', () => {
-    
+    const fetch = jest.fn();
     const dogImage = 'https://images.dog.ceo/breeds/chihuahua/n02085620_3485.jpg';
     const loading = false;
-    
+
     const tree = renderer.create(
       <Image loading={loading}  
         dogImage={dogImage} 
