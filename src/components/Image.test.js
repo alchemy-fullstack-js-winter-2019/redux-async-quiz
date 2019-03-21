f1c83e460a934b22b9ec00d4d7c81c8d31272916
Carmen V Ramos <carmenvramos@yahoo.com>
2019-03-21 12:04:57 -0700

    Modified Image components with loading/fetch

--- src/components/Image.test.js
+++ src/components/Image.test.js
@@ -6,10 +6,12 @@ describe('Image test', () => {
   it('matches a snapshot', () => {
     
     const dogImage = 'https://images.dog.ceo/breeds/chihuahua/n02085620_3485.jpg';
-
+    const loading = false;
+    
     const tree = renderer.create(
-      <Image  
+      <Image loading={loading}  
         dogImage={dogImage} 
+        fetch={fetch}
       />
     ).toJSON();
     expect(tree).toMatchSnapshot();
