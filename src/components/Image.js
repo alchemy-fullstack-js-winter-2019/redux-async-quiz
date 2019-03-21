9b0c90002e53ce6103de4c1d770abedfa9d07178
Kate Dameron <katerj33@gmail.com>
2019-03-21 09:50:08 -0700

    Image comp displays an img with a url prop

---
+++ src/components/Image.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+
+function Image({ url }) {
+  return (
+    <figure>
+      <img src={url} alt="picture of a dog" />
+    </figure>
+  );
+}
+
+Image.propTypes = {
+  url: PropTypes.string.isRequired
+}
