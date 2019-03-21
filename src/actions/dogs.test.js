9ee772cb9fbcd33db6aac363426e6d64f09945e4
Carmen V Ramos <carmenvramos@yahoo.com>
2019-03-21 10:22:27 -0700

    Fixed Actions test

--- src/actions/dogs.test.js
+++ src/actions/dogs.test.js
@@ -6,7 +6,7 @@ jest.mock('../services/dogApi');
 describe('Action test', () => {
   it('handled fetchDog action', () => {
     const fetchedAction = fetchDog();
-    expect(fetchedAction.toEqual({
+    expect(fetchedAction).toEqual({
       type: FETCH_DOG,
       payload: getDog()
     });
