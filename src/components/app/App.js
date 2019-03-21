3049b17b7d6decf92edfac9c1d5e63b3f5e55487
Carmen V Ramos <carmenvramos@yahoo.com>
2019-03-21 10:51:37 -0700

    Added container, modifed store.js

--- src/components/app/App.js
+++ src/components/app/App.js
@@ -1,5 +1,12 @@
 import React from 'react';
+import DogImage from '../../containers/DogImage';
 
 export default function App() {
-  return <h1>Hi</h1>;
+  return (
+    <div>
+      <h1>Dog Image </h1>;
+      <DogImage />
+    </div>
+  );
 }
+
--- src/containers/DogImage.js
+++ src/containers/DogImage.js
@@ -1,1 +1,19 @@
-i
+import Image from '../components/Image';
+import { connect } from 'react-redux';
+import { getDog } from '../selectors/dogs';
+import { fetchDog } from '../actions/dogs';
+
+const mapStateToProps = state => ({
+  dogImage: getDog(state)
+});
+
+const mapDispatchToProps = dispatch => ({
+  fetch() {
+    dispatch(fetchDog());
+  }
+});
+
+export default connect(
+  mapStateToProps,
+  mapDispatchToProps
+)(Image);
