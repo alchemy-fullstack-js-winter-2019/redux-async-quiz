1868aa3a640f6b2e3d6d8e2bcfc5380eeb331c18
Carmen V Ramos <carmenvramos@yahoo.com>
2019-03-21 10:21:02 -0700

    Added action test

---
+++ src/actions/dogs.test.js
@@ -0,0 +1,14 @@
+import { FETCH_DOG, fetchDog } from './dogs';
+import { getDog } from '../services/dogApi';
+
+jest.mock('../services/dogApi');
+
+describe('Action test', () => {
+  it('handled fetchDog action', () => {
+    const fetchedAction = fetchDog();
+    expect(fetchedAction.toEqual({
+      type: FETCH_DOG,
+      payload: getDog()
+    });
+  });
+});
