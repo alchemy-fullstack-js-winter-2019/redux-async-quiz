5ad3e5f3445a74860d6aafc7827c710f1ae692a7
Tyler Corbett <thetylercorbett@gmail.com>
2019-03-21 09:51:53 -0700

    dog can get on screen from initial state

---
+++ src/containers/dogimage/DogImage.js
@@ -0,0 +1,34 @@
+import React, { PureComponent } from 'react';
+import PropTypes from 'prop-types';
+import { connect } from 'react-redux';
+import Image from '../../components/image/Image';
+import { getUrl } from '../../selectors/dogs';
+import { fetchDog } from '../../actions/dogs';
+
+const mapStateToProps = state => ({
+  url: getUrl(state)
+});
+
+// const mapDispatchToProps = dispatch => ({
+//   fetch() {
+//     dispatch(fetchDog());
+//   }
+// });
+
+class DogImage extends PureComponent {
+  static propTypes = {
+    url: PropTypes.string.isRequired,
+    fetch: PropTypes.func
+  }
+  // componentDidMount() {
+  //   this.props.fetch();
+  // }
+  render() {
+    const { url } = this.props;
+    return (
+      <Image url={url}/>
+    );
+  }
+}
+
+export default connect(mapStateToProps, null)(DogImage);
--- src/reducers/dogs.js
+++ src/reducers/dogs.js
@@ -1,9 +1,17 @@
-
+import { FETCH_DOG } from '../actions/dogs';
 
 const initialState = {
   url: 'https://images.dog.ceo/breeds/germanshepherd/n02106662_16129.jpg'
 };
 
 export default function reducer(state = initialState, action) {
-  return state;
+  switch(action.type) {
+    case FETCH_DOG:
+      return {
+        ...state,
+        url: action.payload.message
+      };
+    default:
+      return state;
+  }
 }
