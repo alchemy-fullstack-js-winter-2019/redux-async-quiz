ebc32107aeabd9cb901ae73023688b2240e02c31
Kate Dameron <katerj33@gmail.com>
2019-03-21 09:55:49 -0700

    updated reducer

--- src/reducers/image.js
+++ src/reducers/image.js
@@ -1,7 +1,18 @@
+import { FETCH_DOG } from '../actions/image';
+
 const initialState = {
-  url: ''
+  url: '',
+  loading: false
 };
 
 export default function reducer(state = initialState, { type, payload }) {
-  return state;
+  switch(type) {
+    case FETCH_DOG: 
+      return {
+        url: payload,
+        loading: false
+      };
+    default:
+      return state;
+  }
 }
